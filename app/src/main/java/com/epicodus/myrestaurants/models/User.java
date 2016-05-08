61cbd8ef768472e2e38d7b87f657b03b13e21e4a
Matt Rosanio <mmrosani@syr.edu>
2016-05-08 16:31:25 -0700

    Set up login page

---
+++ app/src/main/java/com/epicodus/myrestaurants/models/User.java
@@ -0,0 +1,24 @@
+package com.epicodus.myrestaurants.models;
+
+/**
+ * Created by Matt on 5/8/2016.
+ */
+public class User {
+    private String name;
+    private String email;
+
+    public User() {}
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
